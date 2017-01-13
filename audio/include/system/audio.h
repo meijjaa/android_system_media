4e706738ddb6b86f3aa31d265122d4b203c072b3
Jian Xu <jian.xu@amlogic.com>
2017-01-13 14:06:12 +0800

    PD#137519: audio: DTS-HD format into raw data
    Change-Id: I9caa1d460ad0c4d12c9da8058dd1efc8050cdd2e
    Signed-off-by: Jian Xu <jian.xu@amlogic.com>

--- audio/include/system/audio.h
+++ audio/include/system/audio.h
@@ -1490,6 +1490,7 @@ static inline bool audio_is_raw_data(audio_format_t format) {
     return (((format & AUDIO_FORMAT_MAIN_MASK) == AUDIO_FORMAT_DTS) ||
         ((format & AUDIO_FORMAT_MAIN_MASK) == AUDIO_FORMAT_AC3) ||
         ((format & AUDIO_FORMAT_MAIN_MASK) == AUDIO_FORMAT_E_AC3)||
+        ((format & AUDIO_FORMAT_MAIN_MASK) == AUDIO_FORMAT_DTS_HD)||
         ((format & AUDIO_FORMAT_MAIN_MASK) == AUDIO_FORMAT_TRUEHD));
 }
 
